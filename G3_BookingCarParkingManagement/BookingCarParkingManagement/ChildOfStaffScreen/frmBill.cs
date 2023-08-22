18f5698f9c654d21a3d19b9f11af6dce9a834105
BaoAnhHo211 <baoanhho.211@gmail.com>
2023-08-22 19:54:35 +0700

    Update sql data + update function show bill

--- G3_BookingCarParkingManagement/BookingCarParkingManagement/ChildOfStaffScreen/frmBill.cs
+++ G3_BookingCarParkingManagement/BookingCarParkingManagement/ChildOfStaffScreen/frmBill.cs
@@ -1,4 +1,5 @@
 ﻿using DataObject.Models;
+using Repository;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -8,16 +9,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Globalization;
 
 namespace BookingCarParkingManagement.ChildOfStaffScreen
 {
     public partial class frmBill : Form
     {
+        CarRepository carRepository = new CarRepository();
         Book book;
+        CultureInfo culture = new CultureInfo("en-US");
         public frmBill(Book book)
         {
             InitializeComponent();
             this.book = book;
+        }
+
+        private void frmBill_Load(object sender, EventArgs e)
+        {
+            Car car = carRepository.GetAll().Single(x => x.CarId == book.CarId);
+            lbCar.Text = car.Bsx;
+            DateTime endTime = (DateTime)book.EndTime;
+            DateTime startTime = (DateTime)book.StartTime;
+            TimeSpan time = endTime.Subtract(startTime);
+            lbHour.Text = time.Hours.ToString() + "Hour "+time.Minutes.ToString()+"Minutes";
+            lbPrice.Text = "40.000/Hour";
+            lbTotal.Text = string.Format(culture, "{0:N0}", decimal.Parse((40000 * Math.Round(time.TotalHours,2)).ToString(), NumberStyles.AllowThousands));
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
